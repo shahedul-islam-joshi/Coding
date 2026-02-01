7689daecdbc9f2e66f33ce5af8f588483c044313
shahedul-islam-joshi <shahedul.islam.joshi.1710@gmail.com>
2026-02-02 01:01:40 +0600

    02_February_2026

--- third_semester/Data Structure Algorithm/Data Structure lab/Queue/queue insert delete.cpp
+++ third_semester/Data Structure Algorithm/Data Structure lab/Queue/queue insert delete.cpp
@@ -1,17 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-queue<int>s;
+#include <iostream>
+#include <queue>
 
-int i,k,n;
-cin>>n;
-for(i=0; i<=n; i++){
-//scanf("%d",&k);
-cin>>k;
-s.push(k);}
-while(!s.empty()){
-//printf("%d\n",s.front());
-cout<<"front:"<<s.front()<<endl;
-s.pop();}
-return 0;
+using namespace std;
+
+int main() {
+    queue<int> s;
+    int i, k, n;
+
+    // 1. Get the number of elements
+    if (!(cin >> n)) return 0;
+
+    // 2. Loop n times (i < n) to push elements
+    for (i = 0; i < n; i++) {
+        cin >> k;
+        s.push(k);
+    }
+
+    // 3. Display and remove elements
+    while (!s.empty()) {
+        cout << "front: " << s.front() << endl;
+        s.pop();
+    }
+
+    return 0;
 }
